27c659d2f3235cc3e2efa19a126fc542fae5b9f4
Yashwanth2408 <yashwanthbalaji.2408@gmail.com>
2025-12-29 03:35:08 +0530

    Configure production backend URL for deployment

--- frontend/lib/graphql/client.ts
+++ frontend/lib/graphql/client.ts
@@ -1,12 +1,30 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
+// Use environment variable in production, localhost in development
+const GRAPHQL_ENDPOINT =
+    process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT ||
+    "http://localhost:8000/graphql";
+
 const httpLink = new HttpLink({
-    uri: "http://localhost:8000/graphql",
+    uri: GRAPHQL_ENDPOINT,
+    credentials: "omit",
 });
 
 const client = new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache(),
+    defaultOptions: {
+        watchQuery: {
+            fetchPolicy: "cache-and-network",
+        },
+        query: {
+            fetchPolicy: "network-only",
+            errorPolicy: "all",
+        },
+        mutate: {
+            errorPolicy: "all",
+        },
+    },
 });
 
 export default client;
